fc5534cd7cfe0e94d0a772a3fdf10f8f66a20eac
AikaTakezawa-810 <aiaikoro810@gmail.com>
2024-11-10 22:03:05 +0900

    再提出

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,10 +1,12 @@
-//変数（体重・身長）の宣言
+//変数（体重・身長・BMI）の宣言
 let bodyWeight;
 let bobyHeight;
+let Bmi;
 
 //値の代入
 bodyWeight = 68;
 bodyHeight = 1.7;
+Bmi = bodyWeight/(bodyHeight*bodyHeight);
 
 //BMIの算出
-console.log(bodyWeight/(bodyHeight*bodyHeight));
+console.log(Bmi);
